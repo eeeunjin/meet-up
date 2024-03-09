32f5f6d71e8f3086dc00a8624a50d31763deb14a
Obebe <shkim000905@naver.com>
2024-03-09 22:29:21 +0900

    login_verification 페이지 생성

--- lib/view/login/login_verification.dart
+++ lib/view/login/login_verification.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:meet_up/util/image.dart';
+import 'package:meet_up/view_model/login/login_view_model.dart';
+import 'package:provider/provider.dart';
 
 class LoginVerification extends StatelessWidget {
   const LoginVerification({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ChangeNotifierProvider(
+      create: (_) => LoginViewModel(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false, // 키보드 오버플로우 해결
+        body: SafeArea(child: _body(context)),
+      ),
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 24.h, left: 9.w),
+          child:
+              // header
+              _header(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _back(BuildContext context) {
+    return GestureDetector(
+      onTap: () => context.pop(), // 뒤로가기
+      child: Image.asset(
+        ImagePath.back,
+        width: 40.w,
+        height: 40.h,
+      ),
+    );
+  }
+
+  Widget _header(BuildContext context) {
+    return Row(
+      children: [
+        _back(context),
+        SizedBox(
+          // 여백
+          width: 119.w,
+        ),
+        Text(
+          '로그인',
+          style: TextStyle(fontSize: 22.sp),
+        ),
+      ],
+    );
   }
 }
