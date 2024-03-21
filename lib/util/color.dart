66fae3e9ccedd2d2c4196b7cd04188af42c317fd
Obebe <shkim000905@naver.com>
2024-03-21 14:27:28 +0900

    checking

--- lib/util/color.dart
+++ lib/util/color.dart
@@ -3,4 +3,12 @@ import 'package:flutter/material.dart';
 class UsedColor {
   // button
   static const Color Button_01 = Color(0xFF564EC2);
+
+  // Gray
+  static const Color Text_1 = Color(0xFF6c6c6c);
+  static const Color Text_2 = Color(0xFF6b6b6b);
+  static const Color Text_3 = Color(0xFF757575);
+  static const Color Text_4 = Color(0xFF888888);
+  static const Color Text_5 = Color(0xFF989898);
+  static const Color Text_6 = Color(0xFFe6e6e6);
 }
