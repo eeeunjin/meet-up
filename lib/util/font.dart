eb874f67a31140ecb830372734259cf82a9caeb3
obebe <shkim000905@naver.com>
2024-03-26 23:19:51 +0900

    login pbone num 텍스트 컬러 수정

--- lib/util/font.dart
+++ lib/util/font.dart
@@ -9,6 +9,12 @@ class AppTextStyles {
         fontSize: 22.sp,
       );
 
+  static TextStyle get PR_SB_24 => TextStyle(
+        fontFamily: 'Pretendard-SB',
+        fontWeight: FontWeight.bold,
+        fontSize: 24.sp,
+      );
+
   static TextStyle get PR_SB_20 => TextStyle(
         fontFamily: 'Pretendard-SB',
         fontSize: 20.sp,
@@ -22,13 +28,25 @@ class AppTextStyles {
   // SUITE
 
   static TextStyle get SU_R_20 => TextStyle(
-        fontFamily: 'Suite',
+        fontFamily: 'SUITE',
         fontWeight: FontWeight.normal,
         fontSize: 20.sp,
       );
 
   static TextStyle get SU_R_12 => TextStyle(
-        fontFamily: 'Suite',
+        fontFamily: 'SUITE',
+        fontWeight: FontWeight.normal,
+        fontSize: 12.sp,
+      );
+
+  static TextStyle get SU_L_24 => TextStyle(
+        fontFamily: 'SUITE-L',
+        fontWeight: FontWeight.normal,
+        fontSize: 24.sp,
+      );
+
+  static TextStyle get SU_L_12 => TextStyle(
+        fontFamily: 'SUITE-L',
         fontWeight: FontWeight.normal,
         fontSize: 12.sp,
       );
