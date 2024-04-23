57e0e7b097a637a850d5989f5ca84d844dbd42f0
Obebe <shkim000905@naver.com>
2024-04-23 16:29:15 +0900

    개인 일정 추가 페이지 작성

--- lib/view/schedule/add_personal_schedule.dart
+++ lib/view/schedule/add_personal_schedule.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:meet_up/util/color.dart';
 import 'package:meet_up/util/font.dart';
 import 'package:meet_up/util/image.dart';
 import 'package:meet_up/view/widget/header_widget.dart';
+import 'package:meet_up/view/widget/next_button.dart';
 import 'package:meet_up/view_model/schedule/schedule_main_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -28,7 +30,8 @@ class AddPersonalSchedule extends StatelessWidget {
                 ),
                 child: _header(context),
               ),
-            _main(context)
+            Expanded(child: _main(context)),
+            _bottom(context),
             // Expanded(
             //   child: SingleChildScrollView(
             //     child: Column(
@@ -82,63 +85,80 @@ class AddPersonalSchedule extends StatelessWidget {
 
   // 구분선
   Widget _divider() {
-    return Container(
+    return Divider(
       height: 0.3.h,
+      // thickness: 0.3.h,
       color: UsedColor.line,
     );
   }
 
   // 메인
   Widget _main(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 22.h),
-        Padding(
-          padding: EdgeInsets.only(left: 23.0.w),
-          child: Row(
-            children: [
-              Image.asset(
-                ImagePath.scheduleIcon1,
-                width: 20.w,
-                height: 20.h,
-              ),
-              SizedBox(width: 20.w),
-              Text(
-                '일정',
-                style: AppTextStyles.PR_M_16
-                    .copyWith(color: UsedColor.charcoal_black),
-              ),
-              SizedBox(width: 16.w),
-              Expanded(
-                child: Transform.translate(
-                  offset: Offset(0, -4.0.h),
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 19.h,
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        isDense: true,
-                        contentPadding: EdgeInsets.zero,
-                        border: InputBorder.none,
-                        hintText: '일정을 입력해주세요',
-                        hintStyle: TextStyle(color: UsedColor.text_5),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 22.h),
+          Padding(
+            padding: EdgeInsets.only(left: 23.0.w),
+            child: Row(
+              children: [
+                Image.asset(
+                  ImagePath.scheduleIcon1,
+                  width: 20.w,
+                  height: 20.h,
+                ),
+                SizedBox(width: 20.w),
+                Text(
+                  '일정',
+                  style: AppTextStyles.PR_M_16
+                      .copyWith(color: UsedColor.charcoal_black),
+                ),
+                SizedBox(width: 16.w),
+                Expanded(
+                  child: Transform.translate(
+                    offset: Offset(0, -4.0.h),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 19.h,
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          isDense: true,
+                          contentPadding: EdgeInsets.zero,
+                          border: InputBorder.none,
+                          hintText: '일정을 입력해주세요',
+                          hintStyle: TextStyle(color: UsedColor.text_5),
+                        ),
+                        style: AppTextStyles.PR_R_15
+                            .copyWith(color: UsedColor.text_5),
                       ),
-                      style: AppTextStyles.PR_R_15
-                          .copyWith(color: UsedColor.text_5),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        SizedBox(height: 22.h),
-        _divider(),
-        SizedBox(height: 18.h),
-        _date(context),
-        SizedBox(height: 18.h),
-        _divider(),
-      ],
+          SizedBox(height: 22.h),
+          _divider(),
+          SizedBox(height: 10.h),
+          _date(context),
+          SizedBox(height: 10.h),
+          _divider(),
+          SizedBox(height: 10.h),
+          _time(context),
+          SizedBox(height: 10.h),
+          _divider(),
+          SizedBox(height: 20.h),
+          _location(context),
+          SizedBox(height: 20.h),
+          _divider(),
+          SizedBox(height: 20.h),
+          _detail(context),
+          SizedBox(height: 18.h),
+          _divider(),
+          SizedBox(height: 18.h),
+          _member(context),
+        ],
+      ),
     );
   }
 
@@ -146,44 +166,12 @@ class AddPersonalSchedule extends StatelessWidget {
     final viewModel =
         Provider.of<ScheduleMainViewModel>(context, listen: false);
 
-    // Mark - ExpansionTile 사용
-    // return Theme(
-    //   data: Theme.of(context).copyWith(
-    //     dividerColor: Colors.transparent,
-    //     unselectedWidgetColor: Colors.white,
-    //   ),
-    //   child: ExpansionTile(
-    //     leading: SizedBox(width: 0.0.w),
-    //     title: Row(
-    //       children: [
-    //         Image.asset(
-    //           ImagePath.scheduleIcon2,
-    //           width: 20.w,
-    //           height: 20.h,
-    //         ),
-    //         SizedBox(width: 10.w),
-    //         Text(
-    //           '날짜',
-    //           style: AppTextStyles.PR_M_16
-    //               .copyWith(color: UsedColor.charcoal_black),
-    //         ),
-    //       ],
-    //     ),
-    //     children: [
-    //       Container(
-    //         height: 132.h,
-    //         color: Colors.green, // ex
-    //       ),
-    //     ],
-    //   ),
-    // );
-
     // Mark - ExpansionPanel 사용
     return Theme(
       data: Theme.of(context).copyWith(cardColor: Colors.white),
       child: ExpansionPanelList(
         elevation: 0.h,
-        expandedHeaderPadding: const EdgeInsets.all(0), // 헤더 주변 기본 패딩 제거
+        expandedHeaderPadding: EdgeInsets.zero, // 헤더 주변 기본 패딩 제거
         expansionCallback: (int index, bool isExpanded) {
           viewModel.toggleDatePanel();
         },
@@ -209,12 +197,25 @@ class AddPersonalSchedule extends StatelessWidget {
                 ),
               );
             },
+            // 토글 내부 내용
             body: SizedBox(
-              height: 150.w,
-              child: const Center(
-                child: Text(
-                  'Selected date content goes here',
-                  style: TextStyle(color: Colors.black),
+              height: 170.h,
+              child: Padding(
+                padding: EdgeInsets.only(top: 8.0.h, left: 70.w, bottom: 34.h),
+                child: Container(
+                  width: 279.w,
+                  height: 132.h,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18.6.r),
+                    border: Border.all(width: 1.w, color: UsedColor.B_line),
+                  ),
+                  // 데이트 피커 넣기
+                  child: const Center(
+                    child: Text(
+                      '데이트 피커 넣기',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -224,5 +225,207 @@ class AddPersonalSchedule extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Widget _time(BuildContext context) {
+    final viewModel =
+        Provider.of<ScheduleMainViewModel>(context, listen: false);
+
+    // Mark - ExpansionPanel 사용
+    return Theme(
+      data: Theme.of(context).copyWith(cardColor: Colors.white),
+      child: ExpansionPanelList(
+        elevation: 0.h,
+        expandedHeaderPadding: EdgeInsets.zero, // 헤더 주변 기본 패딩 제거
+        expansionCallback: (int index, bool isExpanded) {
+          viewModel.toggleTimePanel();
+        },
+        children: [
+          ExpansionPanel(
+            headerBuilder: (BuildContext context, bool isExpanded) {
+              return Padding(
+                padding: EdgeInsets.only(left: 23.0.w),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      ImagePath.scheduleIcon3,
+                      width: 20,
+                      height: 20,
+                    ),
+                    SizedBox(width: 20.w),
+                    Text(
+                      '시간',
+                      style: AppTextStyles.PR_M_16
+                          .copyWith(color: UsedColor.charcoal_black),
+                    ),
+                  ],
+                ),
+              );
+            },
+            body: SizedBox(
+              height: 150.w,
+              child: const Center(
+                child: Text(
+                  'Selected time content goes here',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+            isExpanded:
+                Provider.of<ScheduleMainViewModel>(context).isTimePanelExpanded,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _location(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 23.0.w),
+      child: Row(
+        children: [
+          Image.asset(
+            ImagePath.scheduleIcon4,
+            width: 20.w,
+            height: 20.h,
+          ),
+          SizedBox(width: 20.w),
+          Text(
+            '장소',
+            style:
+                AppTextStyles.PR_M_16.copyWith(color: UsedColor.charcoal_black),
+          ),
+          SizedBox(width: 16.w),
+          Expanded(
+            child: Transform.translate(
+              offset: Offset(0, -4.0.h),
+              child: Container(
+                alignment: Alignment.center,
+                height: 19.h,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.zero,
+                    border: InputBorder.none,
+                    hintText: '만남 장소를 입력해주세요',
+                    hintStyle: TextStyle(color: UsedColor.text_5),
+                  ),
+                  style:
+                      AppTextStyles.PR_R_15.copyWith(color: UsedColor.text_5),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _detail(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 23.0.w),
+      child: Row(
+        children: [
+          Image.asset(
+            ImagePath.scheduleIcon5,
+            width: 20.w,
+            height: 20.h,
+          ),
+          SizedBox(width: 20.w),
+          Text(
+            '설명',
+            style:
+                AppTextStyles.PR_M_16.copyWith(color: UsedColor.charcoal_black),
+          ),
+          SizedBox(width: 16.w),
+          Expanded(
+            child: Transform.translate(
+              offset: Offset(0, -4.0.h),
+              child: Container(
+                alignment: Alignment.center,
+                height: 19.h,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.zero,
+                    border: InputBorder.none,
+                    hintText: '만남에 대한 간단한 설명을 작성해 주세요.',
+                    hintStyle: TextStyle(color: UsedColor.text_5),
+                  ),
+                  style:
+                      AppTextStyles.PR_R_15.copyWith(color: UsedColor.text_5),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _member(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 23.0.w),
+      child: Row(
+        children: [
+          Image.asset(
+            ImagePath.scheduleIcon6,
+            width: 20.w,
+            height: 20.h,
+          ),
+          SizedBox(width: 20.w),
+          Text(
+            '참여',
+            style:
+                AppTextStyles.PR_M_16.copyWith(color: UsedColor.charcoal_black),
+          ),
+          SizedBox(width: 16.w),
+          Expanded(
+            child: Transform.translate(
+              offset: Offset(0, -4.0.h),
+              child: Container(
+                alignment: Alignment.center,
+                height: 19.h,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.zero,
+                    border: InputBorder.none,
+                    hintText: '인원 및 참여자 정보를 입력해주세요.',
+                    hintStyle: TextStyle(color: UsedColor.text_5),
+                  ),
+                  style:
+                      AppTextStyles.PR_R_15.copyWith(color: UsedColor.text_5),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 저장
+  Widget _bottom(BuildContext context) {
+    return Consumer<ScheduleMainViewModel>(
+        builder: (context, viewModel, child) {
+      return Padding(
+        padding: EdgeInsets.only(bottom: 56.0.h, left: 51.w, right: 51.w),
+        child: NextButton(
+          onTap: () {},
+          height: 50.h,
+          text: '저장',
+          enable: viewModel.allCheckCompleted,
+          backgroundColor: viewModel.allCheckCompleted
+              ? UsedColor.button
+              : UsedColor.button_g,
+          textStyle: TextStyle(
+            color:
+                viewModel.allCheckCompleted ? Colors.white : UsedColor.text_2,
+            fontSize: 20.sp,
+          ),
+        ),
+      );
+    });
   }
 }
--- lib/view_model/schedule/schedule_main_view_model.dart
+++ lib/view_model/schedule/schedule_main_view_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ScheduleMainViewModel with ChangeNotifier {
   // Main
+
   // meetUp을 기본 페이지로
   SelectedPart _selectedPart = SelectedPart.meetUp;
 
@@ -21,15 +22,51 @@ class ScheduleMainViewModel with ChangeNotifier {
     }
   }
 
+  // MARK - 개인 일정 추가
+
+  // 일정
+  String _roomNaming = '';
+
+  String get roomNaming => _roomNaming;
+
+  void namingContents(String newNamingCount) {
+    if (_roomNaming != newNamingCount) {
+      _roomNaming = newNamingCount;
+      notifyListeners();
+    }
+  }
+
+  // naming check
+  bool get namingCompleted {
+    return _roomNaming.trim().isNotEmpty;
+  }
+
   // datePicker
   bool _isDatePanelExpanded = false;
 
   bool get isDatePanelExpanded => _isDatePanelExpanded;
 
-  // ExpansionPanel을 토글하는 함수
+  // ExpansionPanel 토글
   void toggleDatePanel() {
-    _isDatePanelExpanded = !_isDatePanelExpanded; // 상태를 반전시킵니다.
-    notifyListeners(); // 위젯들에게 상태 변경을 알립니다.
+    _isDatePanelExpanded = !_isDatePanelExpanded; // 상태 반전
+    notifyListeners();
+  }
+
+  // timePicker
+  bool _isTimePanelExpanded = false;
+
+  bool get isTimePanelExpanded => _isTimePanelExpanded;
+
+  // ExpansionPanel 토글
+  void toggleTimePanel() {
+    _isTimePanelExpanded = !_isTimePanelExpanded;
+    notifyListeners();
+  }
+
+  // check
+
+  bool get allCheckCompleted {
+    return namingCompleted;
   }
 }
 
