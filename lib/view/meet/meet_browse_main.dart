5849f7e2dac95e4625421c308aa645eed5603426
eeeunjin1007@gmail.com <eeeunjin1007@gmail.com>
2024-05-15 22:24:41 +0900

    수정

--- lib/view/meet/meet_browse_main.dart
+++ lib/view/meet/meet_browse_main.dart
@@ -173,7 +173,7 @@ class MeetBrowseMain extends StatelessWidget {
                 meetFilterViewModel.clearAllFilters();
                 meetBrowseViewModel.clearSelectedFilters();
               } else {
-                context.goNamed('meetUserInfo');
+                context.goNamed('meetFilterMain');
               }
             },
             content: Row(
