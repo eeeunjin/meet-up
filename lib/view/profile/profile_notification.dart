56dfbce77701d23b5bd175b1aa355cd3ebc4de3d
eeeunjin1007@gmail.com <eeeunjin1007@gmail.com>
2024-05-28 11:10:35 +0900

    프로필 수정 페이지 작업중

--- lib/view/profile/profile_notification.dart
+++ lib/view/profile/profile_notification.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:meet_up/util/color.dart';
+import 'package:meet_up/util/image.dart';
+import 'package:meet_up/view_model/meet/header_widget.dart';
 
 class ProfileNotification extends StatelessWidget {
   const ProfileNotification({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: 58.h,
+            ),
+            child: _header(context),
+          ),
+          Expanded(child: _main(context)),
+        ],
+      ),
+    );
   }
+
+  // header
+  Widget _header(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          header(back: _back(context), title: '알림'),
+          SizedBox(
+            height: 17.h,
+          ),
+          Divider(
+            thickness: 0.3.h,
+            height: 0.h,
+            color: UsedColor.line,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _back(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.pop();
+      },
+      child: Image.asset(
+        ImagePath.back,
+        width: 10.w,
+        height: 20.h,
+      ),
+    );
+  }
+
+  Widget _main(BuildContext context) {
+    return Container(
+      color: UsedColor.bg_color,
+      child: const Column(
+        children: [
+          // _event(context),
+        ],
+      ),
+    );
+  }
+
+  // Widget _event(BuildContext context) {
+
+  // }
 }
