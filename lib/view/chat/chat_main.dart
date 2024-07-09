75bb49b87d1a8c5da4d544c212186e2f077236f9
jeonmmingu <jeonalsrn@gmail.com>
2024-07-09 17:32:40 +0900

    채팅 탭 / 채팅 뷰 모델 정리

--- lib/view/chat/chat_main.dart
+++ lib/view/chat/chat_main.dart
@@ -1,12 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:meet_up/model/room_model.dart';
 import 'package:meet_up/util/color.dart';
-import 'package:meet_up/view_model/chat/chat_view_model.dart';
+import 'package:meet_up/util/font.dart';
 import 'package:meet_up/view_model/meet/header_widget.dart';
-import 'package:meet_up/view_model/user_view_model.dart';
-import 'package:provider/provider.dart';
 
 class ChatMain extends StatelessWidget {
   const ChatMain({super.key});
@@ -16,6 +13,7 @@ class ChatMain extends StatelessWidget {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: EdgeInsets.only(
@@ -23,7 +21,13 @@ class ChatMain extends StatelessWidget {
             ),
             child: _header(context),
           ),
-          Expanded(child: _main(context)),
+          Expanded(
+            child: Container(
+              width: 1.sw,
+              color: UsedColor.bg_color,
+              child: _main(context),
+            ),
+          ),
         ],
       ),
     );
@@ -48,143 +52,47 @@ class ChatMain extends StatelessWidget {
     );
   }
 
-  // MARK: - 내가 만든 방 / 다른 사람이 만든 방 테스트
   Widget _main(BuildContext context) {
-    final chatViewModel = Provider.of<ChatViewModel>(context);
-    final userViewModel = Provider.of<UserViewModel>(context);
-
-    return StreamBuilder<QuerySnapshot>(
-      // 내가 만든 방
-      // stream: chatViewModel.getMyRoomModel(myUid: userViewModel.uid!),
-
-      // 내가 만든 방을 제외한 다른 사람의 방
-      stream: chatViewModel.getOthersRoomModel(myUid: userViewModel.uid!),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error);
-          return const Text("DB Load Error");
-        } else if (!snapshot.hasData) {
-          return const Text("Has No Data");
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else {
-          print(snapshot.data!.docs.length);
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              // 데이터를 사용하여 원하는 UI를 생성합니다.
-              var data =
-                  snapshot.data!.docs[index].data() as Map<String, dynamic>;
-              var roomModel = RoomModel.fromJson(data);
-              var decodedRoomModel =
-                  chatViewModel.decodingRoomModel(roomModel: roomModel);
-              return ListTile(
-                title: Text(decodedRoomModel.room_name),
-                subtitle: Text(decodedRoomModel.room_age.firstOrNull),
-              );
-            },
-          );
-        }
-      },
-    );
-  }
-
-  // MARK: - 다른 사람이 만든 방 테스트 필터 테스트
-  Widget _filterTest(BuildContext context) {
-    final chatViewModel = Provider.of<ChatViewModel>(context);
-    final userViewModel = Provider.of<UserViewModel>(context);
-
-    return StreamBuilder<QuerySnapshot>(
-      // 내가 만든 방을 제외한 다른 사람의 방
-      stream:
-          chatViewModel.getOthersRoomModelByFilter(myUid: userViewModel.uid!),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error);
-          return const Text("DB Load Error");
-        } else if (!snapshot.hasData) {
-          return const Text("Has No Data");
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else {
-          print(snapshot.data!.docs.length);
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              // 데이터를 사용하여 원하는 UI를 생성합니다.
-              var data =
-                  snapshot.data!.docs[index].data() as Map<String, dynamic>;
-              var roomModel = RoomModel.fromJson(data);
-              var decodedRoomModel = chatViewModel.decodingRoomModel(
-                roomModel: roomModel,
-              );
-              return ListTile(
-                title: Text(decodedRoomModel.room_name),
-                subtitle: Text(decodedRoomModel.room_age.firstOrNull),
-              );
-            },
-          );
-        }
-      },
-    );
-  }
-
-  // MARK: - 다른 사람이 만든 방 상세 정보 출력 테스트
-  Widget _detailInfoTest(BuildContext context) {
-    final chatViewModel = Provider.of<ChatViewModel>(context);
-    final userViewModel = Provider.of<UserViewModel>(context);
-
-    return StreamBuilder<QuerySnapshot>(
-      // 내가 만든 방을 제외한 다른 사람의 방
-      stream: chatViewModel.getOthersRoomModel(myUid: userViewModel.uid!),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error);
-          return const Text("DB Load Error");
-        } else if (!snapshot.hasData) {
-          return const Text("Has No Data");
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else {
-          print(snapshot.data!.docs.length);
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              // 데이터를 사용하여 원하는 UI를 생성합니다.
-              var data =
-                  snapshot.data!.docs[index].data() as Map<String, dynamic>;
-              var roomModel = RoomModel.fromJson(data);
-              var decodeRoomModel = chatViewModel.decodingRoomModel(
-                roomModel: roomModel,
-              );
-              List<DocumentReference> participantDocRefs =
-                  List.empty(growable: true);
-              participantDocRefs.add(decodeRoomModel.room_owner_reference);
-              for (DocumentReference docRef
-                  in decodeRoomModel.room_participant_reference) {
-                participantDocRefs.add(docRef);
-              }
-
-              // 항목을 누르면 참가자 정보가 나오도록 하는 위젯
-              return GestureDetector(
-                onTap: () {
-                  chatViewModel.getParticipantInfo(docRefs: participantDocRefs);
-                },
-                child: ListTile(
-                  title: Text(decodeRoomModel.room_name),
-                  subtitle: Text(decodeRoomModel.room_category),
-                ),
-              );
-            },
-          );
-        }
-      },
+    int itemCount = 3;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.w),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 33.h),
+          Text(
+            '채팅 목록',
+            style: AppTextStyles.PR_SB_20.copyWith(
+              color: UsedColor.charcoal_black,
+            ),
+          ),
+          SizedBox(height: 16.3.h),
+          Expanded(
+            child: ListView.builder(
+              itemCount: itemCount,
+              reverse: false,
+              itemBuilder: (context, index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 80.h,
+                      width: 345.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.r),
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 20.h)
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
