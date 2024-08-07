42e74a8a6076d5b1b92ab2b2c15ffa410b8c696d
jeonmmingu <jeonalsrn@gmail.com>
2024-08-07 23:17:08 +0900

    세로모드만 지원하도록 기본 세팅 변경 (가로모드 제한)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
@@ -37,6 +38,10 @@ Logger logger = Logger();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await initializeFirebase();
   await LoginFunc.autoLogin();
 
