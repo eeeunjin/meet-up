95e20d40013e9f98394f4fdccd956b8152fe4ea0
eeeunjin1007@gmail.com <eeeunjin1007@gmail.com>
2024-11-03 23:31:39 +0900

    알림항목 뷰 구현

--- lib/view/profile/profile_notification.dart
+++ lib/view/profile/profile_notification.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:meet_up/util/color.dart';
+import 'package:meet_up/util/font.dart';
 import 'package:meet_up/util/image.dart';
 import 'package:meet_up/view_model/meet/header_widget.dart';
 
@@ -15,9 +16,7 @@ class ProfileNotification extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.only(
-              top: 58.h,
-            ),
+            padding: EdgeInsets.only(top: 58.h),
             child: _header(context),
           ),
           Expanded(child: _main(context)),
@@ -26,20 +25,20 @@ class ProfileNotification extends StatelessWidget {
     );
   }
 
-  // header
+  // header 부분
   Widget _header(BuildContext context) {
     return Center(
       child: Column(
         children: [
           header(back: _back(context), title: '알림'),
           SizedBox(
-            height: 17.h,
+            height: 16.h,
           ),
           Divider(
             thickness: 0.3.h,
             height: 0.h,
             color: UsedColor.line,
-          )
+          ),
         ],
       ),
     );
@@ -58,18 +57,82 @@ class ProfileNotification extends StatelessWidget {
     );
   }
 
+  // MARK:-main
   Widget _main(BuildContext context) {
+    List<bool> isNewNotification = [true, false, false, false, false, false];
+
     return Container(
-      color: UsedColor.bg_color,
-      child: const Column(
-        children: [
-          // _event(context),
-        ],
+      color: Colors.white,
+      child: ListView.builder(
+        itemCount: isNewNotification.length + 1,
+        itemBuilder: (context, index) {
+          if (index < isNewNotification.length) {
+            // 알림 항목
+            return _notificationItem(context, isNewNotification[index]);
+          } else {
+            // 마지막 항목: 문구
+            return Padding(
+              padding: EdgeInsets.only(
+                top: 40.h,
+                bottom: 110.h,
+              ),
+              child: Center(
+                child: Text(
+                  "알람은 30일 이후 순차적으로 삭제됩니다.",
+                  style: AppTextStyles.SU_R_11.copyWith(
+                    color: UsedColor.text_5,
+                  ),
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
 
-  // Widget _event(BuildContext context) {
-
-  // }
+  // MARK:- 알림 항목
+  Widget _notificationItem(BuildContext context, bool isNew) {
+    return Container(
+      width: 393.w,
+      decoration: BoxDecoration(
+        color: isNew ? UsedColor.image_card : Colors.white,
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(left: 28.w, top: 14.h, bottom: 20.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.asset(ImagePath.profileNotification,
+                    width: 8.w, height: 8.h),
+                SizedBox(width: 2.w),
+                Text(
+                  "이벤트",
+                  style: AppTextStyles.SU_R_10.copyWith(
+                    color: UsedColor.text_3,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              "000 콜라보 이벤트가 시작되었습니다!",
+              style: AppTextStyles.PR_M_18.copyWith(
+                color: isNew ? UsedColor.violet : Colors.black,
+              ),
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              "000하고 00코인 받을 수 있는 이벤트에 참여해 보세요!",
+              style: AppTextStyles.SU_R_11.copyWith(
+                color: UsedColor.text_5,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
